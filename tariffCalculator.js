5e7372bf08d7b8b3d0c3bb3265e56d74a94f13b3
LuchoAGN <102271064+lgarcia-sfisas@users.noreply.github.com>
2024-08-04 22:21:53 -0500

    Create tariffCalculator.js

---
+++ tariffCalculator.js
@@ -0,0 +1,248 @@
+async function fetchProducts() {
+  try {
+    const response = await fetch(
+      "https://shop.saferut.com/shop/products/list_product_saferut"
+    );
+    if (!response.ok) {
+      throw new Error("Error en la respuesta de la red");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
+}
+
+function transformData(products) {
+  const result = { tariffs: {} };
+
+  products.forEach((product) => {
+    const { type, max_invoice, id, price, name, path_img, descr } = product;
+
+    if (!result.tariffs[type]) {
+      result.tariffs[type] = {};
+    }
+
+    if (!result.tariffs[type][max_invoice]) {
+      result.tariffs[type][max_invoice] = [];
+    }
+
+    result.tariffs[type][max_invoice].push({
+      id,
+      price,
+      name,
+      path_img,
+      descr,
+    });
+  });
+
+  for (const type in result.tariffs) {
+    for (const max_invoice in result.tariffs[type]) {
+      while (result.tariffs[type][max_invoice].length < 13) {
+        result.tariffs[type][max_invoice].push({
+          id: null,
+          price: 0,
+          name: "",
+          path_img: "",
+          descr: "",
+        });
+      }
+
+      result.tariffs[type][max_invoice].sort((a, b) => {
+        if (a.price === b.price) {
+          return a.id - b.id;
+        }
+        return a.price - b.price;
+      });
+    }
+  }
+
+  return result;
+}
+
+function populateSelect(selectElement, options) {
+  const fragment = document.createDocumentFragment();
+  options.forEach(({ value, text }) => {
+    const option = document.createElement("option");
+    option.value = value;
+    option.textContent = text;
+    fragment.appendChild(option);
+  });
+  selectElement.appendChild(fragment);
+}
+
+function formatCurrency(value) {
+  return new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+  }).format(value);
+}
+
+function handleCalculateClick(tariffData) {
+  const numFacturaSelect = document.getElementById("NumFactura");
+  const rangoSelect = document.getElementById("Rango");
+
+  const rangeIndex = rangoSelect.selectedIndex;
+  const tariffKey = numFacturaSelect.value;
+
+  const annualElement = document.getElementById("annual");
+  const monthlyElement = document.getElementById("monthly");
+
+  const noneTariff = document.getElementById("none-tariff");
+  const displayTariff = document.getElementById("display-tariff");
+  const buttonCarrito = document.getElementById("add-data-0");
+
+  const type = annualElement.classList.contains("active")
+    ? "annual"
+    : "monthly";
+  if (rangeIndex !== "" && tariffKey !== "") {
+    const tariffValues = tariffData.tariffs[type][tariffKey];
+    const value = tariffValues[rangeIndex - 1];
+
+    if (type === "monthly" && value.price == 0) {
+      console.log(
+        `No se encuentra disponible para mensual este tipo de rango de facturación.`
+      );
+      noneTariff.classList.replace("d-block", "d-none");
+    } else {
+      noneTariff.classList.replace("d-block", "d-none");
+      displayTariff.classList.replace("d-none", "d-block");
+
+      const code_plan = document.getElementById("code_plan");
+      const descr_plan = document.getElementById("descr_plan");
+      const price_plan = document.getElementById("price_plan");
+      const add_cart = document.getElementById("add_cart");
+
+      code_plan.textContent = value.name;
+      descr_plan.textContent = value.descr;
+      price_plan.textContent = formatCurrency(value.price);
+
+      const data = {
+        id: value.id,
+        name: value.name,
+        price: value.price + "",
+        path_img: value.path_img,
+      };
+
+      buttonCarrito.setAttribute("data-product", JSON.stringify(data));
+      add_cart.classList.replace("d-none", "d-flex");
+    }
+  } else {
+    console.log(`Por favor, selecciona un rango y una tarifa.`);
+  }
+}
+
+function setSelect(type) {
+  const annual = document.getElementById("annual");
+  const monthly = document.getElementById("monthly");
+
+  if (type === "annual") {
+    monthly.classList.remove("active");
+    annual.classList.add("active");
+  } else {
+    annual.classList.remove("active");
+    monthly.classList.add("active");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const products = await fetchProducts();
+  const tariffData = transformData(products);
+
+  document.getElementById("content-calcule").innerHTML = `
+      <div class="card">
+        <div class="card-header">
+          <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav-item">
+              <a role="button" id="annual" class="nav-link active" aria-current="true" onclick="setSelect('annual')">Anual</a>
+            </li>
+            <li class="nav-item">
+              <a role="button" id="monthly" class="nav-link" onclick="setSelect('monthly')">Mensual</a>
+            </li>
+          </ul>
+        </div>
+        <div class="card-body text-center">
+          <div class="row row-cols-1 row-cols-md-3">
+            <div class="col text-start mt-2">
+              <label for="NumFactura" class="form-label">Número máximo de facturas</label>
+              <select id="NumFactura" class="form-select" aria-label="NumFactura">
+                <option selected>Selecciona una opción</option>
+              </select>
+            </div>
+            <div class="col text-start mt-2">
+              <label for="Rango" class="form-label">Rango de facturación</label>
+              <select id="Rango" class="form-select" aria-label="Rango">
+                <option selected>Selecciona una opción</option>
+              </select>
+            </div>
+            <div class="col d-flex align-items-center justify-content-center mt-2">
+              <button id="calculateBtn" type="button" class="btn btn-primary">Calcular tarifa</button>
+            </div>
+          </div>
+          <div class="row row-cols-1 row-cols-md mt-4">
+            <div class="col-8 mt-2 detail-tariff p-4">
+              <div id="none-tariff" class="d-block">
+                <p class="fw-light fs-6 text-muted">
+                  Calcule su tarifa para proporcionarle los detalles del plan que prefiera.
+                </p>
+                <div class="d-flex align-items-center justify-content-center">
+                  <dotlottie-player
+                    src="https://lottie.host/f5562867-22ef-463a-9634-7aca26f54940/i2flNWwX0o.lottie"
+                    background="transparent" speed="1" style="width: 220px; height: 220px;" loop autoplay>
+                  </dotlottie-player>
+                </div>
+              </div>
+              <div id="display-tariff" class="d-none">
+                <div class="row">
+                  <img class="w-25" src="/app/assets/img/shops/Shop_2.svg" alt="product-c">
+                </div>
+                <div class="row">
+                  <div class="col text-start">
+                    <p class="m-0">Código del plan:</p>
+                    <p id="code_plan" class="mb-2 text-muted"></p>
+                    <p class="m-0">Descripción:</p>
+                    <p id="descr_plan" class="mb-2 text-muted"></p>
+                  </div>
+                  <div class="col text-start">
+                    <p class="m-0">Precio:</p>
+                    <p id="price_plan" class="mb-2 text-muted"></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="add_cart" class="d-none justify-content-center align-items-center col-4 mt-2">
+              <a id="add-data-0" type="button"
+                class="btn btn-light btn-lg" onclick="addCart('0')">
+                Añadir al carrito
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+  const numFacturaOptions = Object.keys(tariffData.tariffs.monthly)
+    .filter((key) => key !== "0")
+    .sort((a, b) => a - b)
+    .map((key) => ({ value: key, text: parseInt(key).toLocaleString() }));
+
+  numFacturaOptions.push({ value: "0", text: "Máximo" });
+
+  populateSelect(document.getElementById("NumFactura"), numFacturaOptions);
+
+  const rangoOptions = tariffData.ranges.map((range) => {
+    const startFormatted = range.start.toLocaleString("es-CO");
+    const endFormatted =
+      range.end === 0 ? "∞" : range.end.toLocaleString("es-CO");
+    return {
+      value: `${range.start} - ${range.end}`,
+      text: `${startFormatted} - ${endFormatted}`,
+    };
+  });
+
+  populateSelect(document.getElementById("Rango"), rangoOptions);
+
+  document
+    .getElementById("calculateBtn")
+    .addEventListener("click", () => handleCalculateClick(tariffData));
+});
